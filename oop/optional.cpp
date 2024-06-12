685f92a63b0ef5baecfabd2348c2bb664a9a5df8
Hermann Puzhalin <puzhaling@gmail.com>
2024-06-13 03:08:44 +1000

    continue to impl optional

--- oop/optional.cpp
+++ oop/optional.cpp
@@ -1,69 +1,71 @@
+#include <iostream>
 #include <memory>
 #include <concepts>
+#include <exception>
 #include <type_traits>
 
-template< class T > 
-concept Item = !std::is_array_v<T> && !is_lvalue_reference_v<T> && 
-  !is_rvalue_reference_v<T> && std::is_destuctible_v<T>;
-  
-template< Item T >
+template<class T>
+  requires std::destructible<T>  // basic type requirement
 class optional {
  public:
-  using value_type = T;
+  constexpr optional() : has_value_(false) {}; // (1)
+  
+  template<class U = T>
+  constexpr optional(U&& value) {
+  
+  }
 
-  optional(); // (1)
+  ~optional() {
+    if (has_value_ && !std::is_trivially_destructible_v<T>)
+      value_.~T();
+  }
+
+
+  // observers:
+  constexpr T* operator->() noexcept { return &value_; }
+  constexpr T& operator*() & noexcept { return value_; }
+  constexpr explicit operator bool() const noexcept { return has_value_ ? true : false; }
+  constexpr bool has_value() const noexcept { return bool(*this); }
  
-  template< class U = T >
-  optional(U&&); // (8) 
+  constexpr T& value() & {
+    if (!has_value_) 
+      throw std::exception("no value");
+    return value_;
+  }
 
-  ~optional();
+  template<class U>
+  constexpr T value_or(U&& default_value) const& {
+    return bool(*this) ? **this : static_cast<T>(std::forward<U>(default_value));
+    //                  ^^^^ (*this), maybe??
+  }
 
-  template< class U >
-  constexpr optional& operator=(U&&); 
 
-  constexpr T* operator->() noexcept;
-  constexpr T& operator*() & noexcept;
-  constexpr explicit operator bool() const noexcept;
 
-  constexpr bool has_value() const noexcept;
-  constexpr T& value() &;
 
-  template< class U >
-  constexpr T value_or(U&&) const&;
+  // modifiers:
+  
+  // T must be swappable 
+  void swap(optional& other) noexcept {
+    if (!this->has_value_ && !other.has_value())
+      return;
+
+    // in progress...
+    
+  }
+
+  void reset() noexcept {
+    if (has_value_) 
+      value_.~T();
+  }
 
  private:
-  std::unique_ptr<T> value_;
+  union {
+    T value_;
+  };
+  bool has_value_;
 };
 
-typename< Item T >
-optional<T>::optional();
-
-typename< Item T >
-optional<T>::optional() { }
-
-typename< Item T >
-typename< class U = T >
-optional<T>::optional( U&& value ) {
-
-}
-
-template< Item T >
-template< class U = T >
-optional<T>::operator=( U&& value ){
-
-}
-
-template< Item T >
-constexpr T& optional<T>::operator*() & noexcept {
-  return *value_;
-}
-
-template< Item T >
-constexpr T* operator->() noexcept {
- return value_.get();
-}
-
-template< Item T >
-optional<T>::operator bool() const noexcept {
-  return value_;
+int main() {
+  int x = 5;
+  return 0;
 }

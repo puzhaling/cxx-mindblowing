835b49b91e732b51f2e9c3ea57b5e8f5d345847a
Hermann Puzhalin <puzhaling@gmail.com>
2024-06-16 14:53:36 +1000

    add compile time eval template recursive function

---
+++ oop/isprime.cxx
@@ -0,0 +1,30 @@
+#include <iostream>
+
+template<unsigned p, unsigned d>
+struct do_is_prime {
+  static constexpr bool value = (p % d != 0) &&
+                                do_is_prime<p, d - 1>::value;
+};
+
+template<unsigned p>
+struct do_is_prime<p, 2> {
+  static constexpr bool value = (p % 2 != 0);
+};
+
+template<unsigned p>
+struct is_prime {
+  static constexpr bool value = do_is_prime<p, p / 2>::value;
+};
+
+template<>
+struct is_prime<0> { static constexpr bool value = false; };
+template<>
+struct is_prime<1> { static constexpr bool value = false; };
+template<>
+struct is_prime<2> { static constexpr bool value = true; };
+template<>
+struct is_prime<3> { static constexpr bool value = true; };
+
+int main() {
+  std::cout << is_prime<100000123>::value << '\n';
+}

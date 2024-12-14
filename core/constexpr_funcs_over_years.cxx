07a412eff817aa8662631fa603f14de6751507ed
Hermann Puzhalin <puzhaling@gmail.com>
2024-12-14 21:10:12 +1000

    update 'constexpr_funcs_over_years.cxx'

--- core/constexpr_funcs_over_years.cxx
+++ core/constexpr_funcs_over_years.cxx
@@ -22,3 +22,16 @@ constexpr int pow(int base, int exp) noexcept
 
 // "The constructors with a constexpr specifier make their
 // type a literal type"
+
+
+
+
+// UPDATE:
+// in c++11 2 restrictions does not allow to declare setters
+// as constexpr functions:
+// 1) in c++11 constexpr member functions are implicitly const
+// 2) they have void return type and void does not considered
+//    as literal type in c++11
+
+// all the above restrictions are removed in c++14
+
